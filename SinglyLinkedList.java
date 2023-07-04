677b05554212c6ee395e7e9d34fb4a9971bb16d7
Hanck1215 <st11025212@gmail.com>
2023-07-04 12:02:22 +0800

    把單向鏈結串列剩下的功能定義好

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,4 +1,4 @@
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> {
     
     //------------------nested Node class--------------------
     private static class Node<E> {
@@ -16,4 +16,51 @@ public class SinglyLinkedList {
     }
     //---------------end of nested Node class----------------
 
+    private Node<E> head = null ;
+    private Node<E> tail = null ;
+    private int size = 0 ;
+
+    public SinglyLinkedList() {} //Constructs an initially empty list
+
+    public int size() { return this.size ; }
+    public boolean isEmpty() { return this.size == 0 ; }
+
+    public E first() {
+        if (this.isEmpty()) { return null ; }
+        return head.getElement() ;
+    }
+
+    public E last() {
+        if (this.isEmpty()) { return null ; }
+        return this.tail.getElement() ;
+    }
+
+    public void addFirst(E e) {
+        this.head = new Node<E>(e, head) ;
+        if( this.size == 0 ) { this.tail = this.head ; }
+        size++ ;
+    }
+
+    public void addLast(E e) {
+        Node<E> newest = new Node<>(e, null) ;
+        if(this.isEmpty()) {
+            head = newest ;
+        }else {
+            this.tail.setNext(newest) ;
+        }
+        this.tail = newest ;
+        size++ ;
+    }
+
+    public E removeFirst() {
+        if(this.isEmpty()) { return null ; }
+        E answer = this.head.getElement() ;
+        this.head = this.head.getNext() ;
+        this.size-- ;
+
+        if(this.isEmpty()) {
+            this.tail = null ;
+        }
+        return answer ;
+    }
 }
