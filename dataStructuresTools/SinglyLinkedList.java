20a9d5a0f97a121f3a592f02d3b989df6bf88941
Hanck1215 <st11025212@gmail.com>
2023-07-04 12:15:13 +0800

    創建一個main package，創建一個dataStructuresTools package

--- dataStructuresTools/SinglyLinkedList.java
+++ dataStructuresTools/SinglyLinkedList.java
@@ -1,3 +1,5 @@
+package dataStructuresTools ;
+
 public class SinglyLinkedList<E> {
     
     //------------------nested Node class--------------------
---
+++ main/main.java
@@ -0,0 +1,8 @@
+package main ;
+import dataStructuresTools.SinglyLinkedList ;
+
+public class main {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList<>() ;
+    }
+}
