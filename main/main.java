be41a9597135f3d91d71676c05209edf40d1f37f
Hanck1215 <st11025212@gmail.com>
2023-07-04 12:18:15 +0800

    成功印出list的第一個element

--- main/main.java
+++ main/main.java
@@ -5,5 +5,6 @@ public class main {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList<>() ;
         list.addFirst("Hanck");
+        System.out.println(list.first()) ;
     }
 }
