625a081fbb6594f6cca591deb077e84114378df7
Hanck1215 <st11025212@gmail.com>
2023-07-04 12:21:45 +0800

    測試結果均符合預期

--- main/main.java
+++ main/main.java
@@ -6,5 +6,16 @@ public class main {
         SinglyLinkedList list = new SinglyLinkedList<>() ;
         list.addFirst("Hanck");
         System.out.println(list.first()) ;
+        list.addFirst(true);
+        System.out.println(list.first()) ;
+        list.addLast(456) ;
+        System.out.println(list.last()) ;
+        System.out.println(list.size()) ;
+        list.removeFirst() ;
+        list.removeFirst() ;
+        System.out.println(list.first()) ;
+        System.out.println(list.last()) ;
+
+
     }
 }
