25431e7a39f83e71a0b8844ccd8c04b38661ffaa
Hanck1215 <st11025212@gmail.com>
2023-07-04 12:16:53 +0800

    嘗試替串列新增一個element，'Hanck'

--- main/main.java
+++ main/main.java
@@ -4,5 +4,6 @@ import dataStructuresTools.SinglyLinkedList ;
 public class main {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList<>() ;
+        list.addFirst("Hanck");
     }
 }
